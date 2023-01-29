e809dd6387147056519e938cfbc16a86c9e14383
ejaman <leegm17@naver.com>
2023-01-29 11:54:52 +0900

    feat: 드래그 앤 드롭 구현

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,205 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const initialLists = [
+  {
+    title: "lecture1",
+    process: [{ title: "activity1" }],
+  },
+  {
+    title: "lecture2",
+    process: [{ title: "activity1" }, { title: "activity2" }],
+  },
+  {
+    title: "lecture3",
+    process: [
+      { title: "activity1" },
+      { title: "activity2" },
+      { title: "activity3" },
+    ],
+  },
+];
+
+const initialDragData = {
+  target: null,
+  index: -1,
+  move_down: [],
+  move_up: [],
+  updateLists: [],
+};
+interface ITodo {
+  title: string;
+  process: { title: string }[];
+}
+
+interface InitialDragData {
+  target: any;
+  index: number;
+  move_down: number[];
+  move_up: number[];
+  updateLists: ITodo[];
+}
 
 const Home = () => {
-  return <div>Home</div>;
+  const [lists, setLists] = useState<ITodo[]>(initialLists);
+  const [dragData, setDragData] = useState<InitialDragData>(initialDragData);
+  const [isDragged, setIsDragged] = useState(false);
+
+  const onDragStart = (e: React.DragEvent<HTMLElement>): void => {
+    setIsDragged(true);
+    setDragData({
+      ...dragData,
+      target: e.target,
+      index: Number(e.currentTarget.dataset.index),
+      updateLists: [...lists],
+    });
+
+    e.dataTransfer.setData("text/html", "");
+    e.dataTransfer.effectAllowed = "move";
+  };
+  const onDragEnter = (e: React.DragEvent<HTMLElement>): void => {
+    const _dragged = Number(dragData.target.dataset.index); // 처음 잡은 값
+    const _index = Number(dragData.index); // 바로 전 값
+    const _target = Number(e.currentTarget.dataset.index); // 현재 값
+    let move_down = [...dragData.move_down];
+    let move_up = [...dragData.move_up];
+
+    let data = [...dragData.updateLists];
+    data[_index] = data.splice(_target, 1, data[_index])[0];
+    if (_dragged > _target) {
+      move_down.includes(_target) ? move_down.pop() : move_down.push(_target);
+    } else if (_dragged < _target) {
+      move_up.includes(_target) ? move_up.pop() : move_up.push(_target);
+    } else {
+      move_down = [];
+      move_up = [];
+    }
+
+    setDragData({
+      ...dragData,
+      updateLists: data,
+      index: _target,
+      move_up,
+      move_down,
+    });
+  };
+  const onDragLeave = (e: React.DragEvent<HTMLElement>): void => {
+    if (e.target === dragData.target) {
+      e.currentTarget.style.visibility = "hidden";
+    }
+  };
+  const onDragEnd = (e: React.DragEvent<HTMLElement>): void => {
+    setIsDragged(false);
+    setLists([...dragData.updateLists]);
+
+    setDragData({
+      ...dragData,
+      move_down: [],
+      move_up: [],
+      updateLists: [],
+    });
+
+    e.currentTarget.style.visibility = "visible";
+    e.dataTransfer.dropEffect = "move";
+  };
+
+  const stopDragging = (e: any) => {
+    e.stopPropagation();
+    e.preventDefault();
+    return true;
+  };
+
+  return (
+    <Container>
+      <List className="list" onDragOver={stopDragging}>
+        {lists.map((e, i) => {
+          let default_class = "";
+
+          dragData.move_down.includes(i) && (default_class = "move_down");
+          dragData.move_up.includes(i) && (default_class = "move_up");
+
+          return (
+            <ListItem
+              key={i}
+              data-index={i}
+              draggable
+              onDragStart={onDragStart}
+              onDragEnter={onDragEnter}
+              onDragLeave={onDragLeave}
+              onDragEnd={onDragEnd}
+              className={default_class}
+              isDragged={isDragged}
+            >
+              <p>{e.title}</p>
+              {e.process.map((e, i) => {
+                return (
+                  <ListItem draggable isDragged={isDragged} key={i}>
+                    {e.title}
+                  </ListItem>
+                );
+              })}
+            </ListItem>
+          );
+        })}
+      </List>
+    </Container>
+  );
 };
 
 export default Home;
+
+const Container = styled.div`
+  background-color: lightgray;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  * {
+    padding: 0;
+    margin: 0;
+  }
+`;
+
+const List = styled.ul`
+  width: 300px;
+  list-style: none;
+  background-color: #bf4949;
+  border-radius: 5px;
+  display: flex;
+  flex-direction: column;
+`;
+
+interface IListItem {
+  isDragged: boolean;
+}
+
+const ListItem = styled.div<IListItem>`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  padding: 15px 8px;
+  ${(props) => props.isDragged && "transition: transform 200ms ease 0s"};
+  user-select: none;
+  touch-action: none;
+  background-color: white;
+  opacity: 0.5;
+  cursor: grab;
+  i {
+    flex: 1;
+  }
+  p {
+    flex: 6;
+  }
+  &.move_up {
+    transform: translate(0, -100px);
+    z-index: 1;
+  }
+  &.move_down {
+    transform: translate(0, 100px);
+    z-index: 1;
+  }
+  & > * {
+    pointer-events: none;
+  }
+`;
--- src/pages/Todo.tsx
+++ src/pages/Todo.tsx
@@ -1,7 +1,172 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import styled from "styled-components";
+
+interface ITodo {
+  main: string;
+  images: string[];
+}
+interface InitialDragData {
+  move_left: number[];
+  move_right: number[];
+  updateLists: ITodo;
+}
+
+const initialLists = {
+  main: "main",
+  images: ["image1", "image2", "image3", "image4", "image5"],
+};
 
 const Todo = () => {
-  return <div>todo</div>;
+  const [thumbnail, setThumbnail] = useState<ITodo>(initialLists);
+  const [dragData, setDragData] = useState<InitialDragData>({
+    move_left: [],
+    move_right: [],
+    updateLists: { ...thumbnail },
+  });
+  const [isDragged, setIsDragged] = useState(false);
+  const clickedItem = useRef<number>(-1);
+
+  const onDragStart = (e: React.DragEvent<HTMLElement>): void => {
+    setIsDragged(true);
+    clickedItem.current = Number(e.currentTarget.dataset.index);
+    e.dataTransfer.effectAllowed = "move";
+  };
+
+  const onDragEnter = (e: React.DragEvent<HTMLElement>): void => {
+    const clickedIndex = clickedItem.current;
+    const target = Number(e.currentTarget.dataset.index);
+    let move_left = [...dragData.move_left];
+    let move_right = [...dragData.move_right];
+
+    let data = [thumbnail.main, ...thumbnail.images];
+    const clicked = data.splice(clickedIndex, 1);
+    data.splice(target, 0, ...clicked);
+
+    if (clickedIndex > target) {
+      move_left.includes(target) ? move_left.pop() : move_left.push(target);
+    } else if (clickedIndex < target) {
+      move_right.includes(target) ? move_right.pop() : move_right.push(target);
+    } else {
+      move_left = [];
+      move_right = [];
+    }
+    e.currentTarget.style.cursor = "grabbing";
+    setDragData({
+      move_left,
+      move_right,
+      updateLists: { main: data[0], images: data.slice(1) },
+    });
+  };
+
+  const onDragLeave = (e: React.DragEvent<HTMLElement>): void => {
+    const target = Number(e.currentTarget.dataset.index);
+    if (target === clickedItem.current) {
+      e.currentTarget.style.visibility = "hidden";
+    }
+  };
+
+  const onDragEnd = (e: React.DragEvent<HTMLElement>): void => {
+    setIsDragged(false);
+    setThumbnail({ ...dragData.updateLists });
+
+    clickedItem.current = -1;
+    setDragData({
+      move_left: [],
+      move_right: [],
+      updateLists: { main: "", images: [] },
+    });
+
+    e.currentTarget.style.visibility = "visible";
+    e.dataTransfer.dropEffect = "move";
+  };
+
+  const stopDragging = (e: any) => {
+    e.stopPropagation();
+    e.preventDefault();
+    return true;
+  };
+
+  return (
+    <Container>
+      <List className="list" onDragOver={stopDragging}>
+        {[thumbnail.main, ...thumbnail.images].map((e, i) => {
+          let default_class = "";
+          dragData.move_left.includes(i) && (default_class = "move_left");
+          dragData.move_right.includes(i) && (default_class = "move_right");
+
+          return (
+            <ListItem
+              key={i}
+              data-index={i}
+              className={default_class}
+              isDragged={isDragged}
+              draggable
+              onDragStart={onDragStart}
+              onDragEnter={onDragEnter}
+              onDragLeave={onDragLeave}
+              onDragEnd={onDragEnd}
+            >
+              <p>{e}</p>
+            </ListItem>
+          );
+        })}
+      </List>
+    </Container>
+  );
 };
 
 export default Todo;
+
+const Container = styled.div`
+  background-color: lightgray;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  * {
+    padding: 0;
+    margin: 0;
+  }
+`;
+
+const List = styled.ul`
+  width: 800px;
+  list-style: none;
+  background-color: #bf4949;
+  border-radius: 5px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+`;
+
+interface IListItem {
+  isDragged: boolean;
+}
+
+const ListItem = styled.div<IListItem>`
+  align-items: center;
+  width: 100px;
+  height: 50px;
+  ${(props) => props.isDragged && "transition: transform 200ms ease 0s"};
+  user-select: none;
+  touch-action: none;
+  background-color: white;
+  opacity: 0.5;
+  cursor: grab;
+  i {
+    flex: 1;
+  }
+  p {
+    flex: 6;
+  }
+  &.move_right {
+    transform: translate(-100px, 0);
+  }
+  &.move_left {
+    transform: translate(100px, 0);
+  }
+  & > * {
+    pointer-events: none;
+  }
+`;
